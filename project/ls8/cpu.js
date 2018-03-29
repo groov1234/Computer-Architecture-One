e0b56535cdb1eca18a55a9f714fd47109f14890e
groov1234 <c.groover@outlook.com>
2018-03-29 14:54:59 -0400

    PUSH/POP/CALL/RET

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -6,6 +6,7 @@
  * Class for simulating a simple Computer (CPU & memory)
  */
 class CPU {
+
     /**
      * Initialize the CPU
      */
@@ -13,17 +14,18 @@ class CPU {
         this.ram = ram;
 
         this.reg = new Array(8).fill(0); // General-purpose registers R0-R7
-
+        
         // Special-purpose registers
         this.reg.PC = 0; // Program Counter
-    }
-
+        this.reg[7] = 0xF4; // Stack Pointer
+    };
+	
     /**
      * Store value in memory address, useful for program loading
      */
     poke(address, value) {
         this.ram.write(address, value);
-    }
+    };
 
     /**
      * Starts the clock ticking on the CPU
@@ -34,14 +36,14 @@ class CPU {
         this.clock = setInterval(() => {
             _this.tick();
         }, 1); // 1 ms delay == 1 KHz clock == 0.000001 GHz
-    }
+    };
 
     /**
      * Stops the clock
      */
     stopClock() {
         clearInterval(this.clock);
-    }
+    };
 
     /**
      * ALU functionality
@@ -58,8 +60,11 @@ class CPU {
             case 'MUL':
                 return this.reg[regA] * this.reg[regB];
                 break;
-        }
-    }
+            case 'ADD':
+                return this.reg[regA] + this.reg[regB];
+                break;
+        };
+    };
 
     /**
      * Advances the CPU one cycle
@@ -83,39 +88,62 @@ class CPU {
         // Execute the instruction. Perform the actions for the instruction as
         // outlined in the LS-8 spec.
 
+        // mnemonics for the instructions
         const LDI = 0b10011001;
         const PRN = 0b01000011;
         const MUL = 0b10101010;
+        const ADD = 0b10101000;
+        const PUSH = 0b01001101;
+        const POP = 0b01001100;
+        const CALL = 0b01001000;
+        const RET = 0b00001001;
         const HLT = 0b00000001;
 
         // handlers for the functionality of each instruction
-        const handle_LDI = (operandA, operandB) => {
-            this.reg[operandA] = operandB;
+        const handle_LDI = (register, value) => {
+            this.reg[register] = value;
+        };    
+        const handle_PRN = (register) => {
+            console.log(this.reg[register]);
+        };    
+        const handle_MUL = (registerA, registerB) => {
+            this.reg[registerA] = this.alu('MUL', registerA, registerB);
         };
-        const handle_PRN = operandA => {
-            console.log(this.reg[operandA]);
+        const handle_ADD = (registerA, registerB) => {
+            this.reg[registerA] = this.alu('ADD', registerA, registerB);
         };
-        const handle_MUL = (operandA, operandB) => {
-            // this.reg[operandA] = this.reg[operandA] * this.reg[operandB];
-            this.reg[operandA] = this.alu('MUL', operandA, operandB);
+        const handle_PUSH = (register) => {
+            this.ram.write(--this.reg[7], this.reg[register]);
+        };
+        const handle_POP = (register) => {
+            this.reg[register] = this.ram.read(this.reg[7]++);
+        };
+        const handle_CALL = (register) => {
+            this.ram.write(--this.reg[7], this.reg.PC + 2);
+            this.reg.PC = this.reg[register];
+        };
+        const handle_RET = () => {
+            this.reg.PC = this.ram.read(this.reg[7]++);
         };
         const handle_HLT = () => this.stopClock();
 
         // handler for invalid instruction
-        const handle_invalid_instruction = instruction => {
-            console.log(
-                `${instruction.toString(
-                    2
-                )} is not a valid instruction; halting operation.`
-            );
+        const handle_invalid_instruction = (instruction) => {
+            console.log(`${instruction.toString(2)} is not a valid instruction; halting operation.`);
             handle_HLT();
-        };
+        };    
 
+        // branch table to pair mnemonics with functions
         const branchTable = {
             [LDI]: handle_LDI,
             [PRN]: handle_PRN,
             [MUL]: handle_MUL,
-            [HLT]: handle_HLT
+            [ADD]: handle_ADD,
+            [PUSH]: handle_PUSH,
+            [POP]: handle_POP,
+            [CALL]: handle_CALL,
+            [RET]: handle_RET,
+            [HLT]: handle_HLT,
         };
 
         // call the function if it is in the branch table or handle invalid instruction
@@ -123,16 +151,21 @@ class CPU {
             branchTable[IR](operandA, operandB);
         } else {
             handle_invalid_instruction(IR);
-        }
+        };
 
         // Increment the PC register to go to the next instruction. Instructions
         // can be 1, 2, or 3 bytes long. Hint: the high 2 bits of the
         // instruction byte tells you how many bytes follow the instruction byte
         // for any particular instruction.
-
-        // !!! IMPLEMENT ME
-        this.reg.PC += (IR >>> 6) + 1;
-    }
-}
+        switch (IR) {
+            case CALL:
+            case RET:
+              break;
+            default:
+              this.reg.PC += (IR >>> 6) + 1;
+              break;
+        }
+    };
+};
 
 module.exports = CPU;
--- project/ls8/ls8.js
+++ project/ls8/ls8.js
@@ -1,40 +1,43 @@
 const RAM = require('./ram');
 const CPU = require('./cpu');
+const fs = require('fs');
 
 /**
  * Load an LS8 program into memory
  *
  * TODO: load this from a file on disk instead of having it hardcoded
  */
-function loadMemory() {
-    // Hardcoded program to print the number 8 on the console
+function loadMemory(cpu) {
+  let program = [];
+  if (process.argv.length === 3) {
+    const lineReader = require('readline').createInterface({
+      input: fs.createReadStream(process.argv[2])
+    });
 
-    // const program = [ // print8.ls8
-    //     "10011001", // LDI R0,8  Store 8 into R0
-    //     "00000000",
-    //     "00001000",
-    //     "01000011", // PRN R0    Print the value in R0
-    //     "00000000",
-    //     "00000001"  // HLT       Halt and quit
-    // ];
-    const program = [
-        "10011001", //LDI R0,8  Load R0 with value 8
-        "00000000",
-        "00001000",
-        "10011001", //LDI R1,9  Load R1 with value 9
-        "00000001",
-        "00001001",
-        "10101010", //MUL R0,R1 Multiply R0*R1, storing result in R0
-        "00000000",
-        "00000001",
-        "01000011", //PRN R0    Print value in R0
-        "00000000",
-        "00000001" //HLT       Halt
-    ];
-    // Load the program into the CPU's memory a byte at a time
-    for (let i = 0; i < program.length; i++) {
-        cpu.poke(i, parseInt(program[i], 2));
-    }
+    lineReader.on('line', function(line) {
+      let str = line.split('#')[0].slice(0, 8);
+
+      if (str.length > 1) {
+        program.push(str);
+      } else {
+        return;
+      }
+    });
+
+    lineReader.on('close', function() {
+      processProgram(program, cpu);
+    });
+  } else {
+    console.log('Please include an .ls8 file name to execute.');
+  }
+}
+
+//
+function processProgram(arr, cpu) {
+  for (let i = 0; i < arr.length; i++) {
+    cpu.poke(i, parseInt(arr[i], 2));
+  }
+  cpu.startClock();
 }
 
 /**
@@ -47,5 +50,3 @@ let cpu = new CPU(ram);
 // TODO: get name of ls8 file to load from command line
 
 loadMemory(cpu);
-
-cpu.startClock();
